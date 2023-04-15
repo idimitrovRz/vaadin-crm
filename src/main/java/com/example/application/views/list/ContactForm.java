3196b0aeb0ead51e5018237ad993f06805151055
Ivan Dimitrov <idimitrov@devexperts.com>
2023-04-15 11:42:55 +0300

    Project init
    - add Entities
    - add all dependencies
    - add simple Vaadin UI

---
+++ src/main/java/com/example/application/views/list/ContactForm.java
@@ -0,0 +1,49 @@
+package com.example.application.views.list;
+
+import com.example.application.data.entity.Company;
+import com.example.application.data.entity.Status;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
+
+import java.util.List;
+
+public class ContactForm extends FormLayout {
+    TextField firstName = new TextField("First name");
+    TextField lastName = new TextField("Last name");
+    EmailField email = new EmailField("Email");
+    ComboBox<Status> status = new ComboBox<>("Status");
+    ComboBox<Company> company = new ComboBox<>("Company");
+    Button saveButton = new Button("Save");
+    Button deleteButton = new Button("Delete");
+    Button cancelButton = new Button("Cancel");
+
+    public ContactForm(List<Company> companies, List<Status> statuses) {
+        addClassNames("contact-form");
+
+        company.setItems(companies);
+        company.setItemLabelGenerator(Company::getName);
+
+        status.setItems(statuses);
+        status.setItemLabelGenerator(Status::getName);
+
+        add(firstName, lastName, email, company, status, createButtonLayout());
+    }
+
+    private Component createButtonLayout() {
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        deleteButton.addThemeVariants(ButtonVariant.LUMO_ERROR);
+        cancelButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+
+        saveButton.addClickShortcut(Key.ENTER);
+        cancelButton.addClickShortcut(Key.ESCAPE);
+
+        return new HorizontalLayout(saveButton, deleteButton, cancelButton);
+    }
+}
